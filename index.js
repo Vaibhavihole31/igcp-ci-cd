8d7f51d12d2240a199aaf0136e761fc8070948e9
Vaibhavihole05 <itspinki05@gmail.com>
2023-12-18 08:22:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", async(req, res)=>{
+    res.send(`<h1>All Good </h1>`)
+})
+
+app.get("/test", (req, res)=>{
+    res.send("<h1>Hey I am Working....</h1>")
+})
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`)
+});
